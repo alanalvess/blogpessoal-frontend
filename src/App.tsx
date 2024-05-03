d45b9b142a25ed6ba6c3dc748b1b5eb39457bff9
Alan Alves <alan.alvess@outlook.com>
2024-05-03 14:10:47 -0300

    Implementando Axios Context API

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 
@@ -18,6 +20,8 @@ function App() {
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
